c78ec519fc737a2d940c06ddc59f918726ef799f
Tau <lorenzo.tausani@studenti.unipd.it>
2024-02-13 15:58:24 +0100

    change: added selectivity to .pt/pth in get_net_paths

--- zdream/net_utils.py
+++ zdream/net_utils.py
@@ -2,12 +2,23 @@ from pathlib import Path
 import os
     
 def get_net_paths(base_nets_dir='/content/drive/MyDrive/XDREAM'):
-  nets_dict = {'base_nets_dir':Path(base_nets_dir)}
-  for root, _, files in os.walk(base_nets_dir):
-      for f in files:
-          file_path = os.path.join(root, f)
-          nets_dict[f] = file_path
-  return nets_dict
+    """
+    Retrieves the paths of the files of the weights of pytorch neural nets within a base directory and returns a dictionary
+    where the keys are the file names and the values are the full paths to those files.
+
+    Args:
+        base_nets_dir (str): The path of the base directory (i.e. the dir that contains all nn files). Default is '/content/drive/MyDrive/XDREAM'.
+
+    Returns:
+        Dict[str, str]: A dictionary where the keys are the nn file names and the values are the full paths to those files.
+    """
+    nets_dict = {'base_nets_dir':Path(base_nets_dir)}
+    for root, _, files in os.walk(base_nets_dir):
+        for f in files:
+          if f.lower().endswith(('.pt', '.pth')):
+            file_path = os.path.join(root, f)
+            nets_dict[f] = file_path
+    return nets_dict
 
 
 
