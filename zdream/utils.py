0d2c54db23e4153934e57ac5bf3475bb49c759c8
Sebastiano Quintavalle <878500@stud.unive.it>
2024-02-13 16:41:33 +0100

    feat: added Stimuli type

--- zdream/utils.py
+++ zdream/utils.py
@@ -1,13 +1,19 @@
 import numpy as np
 from typing import TypeVar, Callable, Dict
-from numpy.typing import NDArray, ArrayLike
+from numpy.typing import NDArray
+from torch import Tensor
+from PIL.Image import Image
 
+# Type Generics
 T = TypeVar('T')
 D = TypeVar('D')
 
+# Type Aliases
+Stimuli = Tensor | Image
 SubjectState = NDArray | Dict[str, NDArray]
 ObjectiveFunction = Callable[[SubjectState], NDArray[np.float32]]
 
+# Type function utils
 def default(var : T | None, val : D) -> T | D:
     return val if var is None else var
 
