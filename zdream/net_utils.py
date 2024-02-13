0b610086e34b974f72bad4453bf90cafd80fee5e
Tau <lorenzo.tausani@studenti.unipd.it>
2024-02-13 15:19:35 +0100

    change: net_utils simplification w.r.t. Kreimann

--- zdream/net_utils.py
+++ zdream/net_utils.py
@@ -1,6 +1,13 @@
 from pathlib import Path
+import os
+    
+def get_net_paths(base_nets_dir='/content/drive/MyDrive/XDREAM'):
+  nets_dict = {'base_nets_dir':Path(base_nets_dir)}
+  for root, _, files in os.walk(base_nets_dir):
+      for f in files:
+          file_path = os.path.join(root, f)
+          nets_dict[f] = file_path
+  return nets_dict
 
-# whether gpu is available for NN computation
-gpu_available = True
-# root directory where network defs and weights are stored; see net_catalogue.py
-nets_dir = Path('/content/drive/MyDrive/XDREAM')
+
+
