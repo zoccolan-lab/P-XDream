dbc44b4c933579489b950c150ddd8eb63d16f2b8
Sebastiano Quintavalle <878500@stud.unive.it>
2024-02-19 18:24:31 +0100

    refactor: kreiman vs custom image reconstruction

--- script/inverse_generation.py
+++ script/inverse_generation.py
@@ -1,6 +1,22 @@
-from torch import nn
+from os import path
+import numpy as np
+import torch
+from torch import Tensor
+import torch.nn as nn
+from PIL import Image
+from einops import rearrange
+from torchvision.models import list_models
+from torchvision.utils import make_grid
+from torchvision.transforms import ToPILImage
+from typing import Any, Dict, List, cast, Tuple
+from numpy.typing import NDArray
 
+from zdream.generator import InverseAlexGenerator
+from zdream.subject import NetworkSubject
+from zdream.utils import Message, SubjectState, read_json, device
+from zdream.probe import RecordingProbe
 
+# ----------------------------------------- KREIMAN --------------------------------------
 class DeePSiMNorm(nn.Module):
     _layer1_ios = (96, 128, 2)
 
@@ -163,3 +179,171 @@ class DeePSiMFc(nn.Module):
 
 class DeePSiMFc8(DeePSiMFc):
     _num_inputs = 1000
+# -------------------------------------------------------------------------------------
+
+def instantiate_network(network_name: str, layer_name: str) -> NetworkSubject:
+    
+    rec_probe = RecordingProbe(target = {layer_name: None}) # Record all activations
+    sbj_net = NetworkSubject(record_probe=rec_probe, network_name=network_name)
+    sbj_net._network.eval()
+    
+    return sbj_net
+
+def load_test_image(image_path: str, image_size: Tuple[int, int]) -> Tensor:
+    
+    img = Image.open(image_path).convert("RGB")
+    img = np.asarray(img.resize(image_size)) / 255.
+    img = torch.tensor(rearrange(img, 'h w c -> 1 c h w'))
+    img = img.to(device)
+    
+    return img
+
+def get_activations(sbj_net: NetworkSubject, img: Tensor) -> SubjectState:
+
+    message = Message(mask=np.ones(img.shape[0], dtype=bool))
+    out, _ = sbj_net.forward(data = (img, message))
+
+    return out
+
+def get_custom_generator(weights_path: str, variant: str) -> nn.Module:
+
+    generator = InverseAlexGenerator(root=weights_path, variant=variant)
+    generator.to(device=device)
+    generator.eval()
+    
+    return generator
+
+def get_external_generator(weights_path: str, variant: str) -> nn.Module:
+    
+    if variant == 'fc8':
+        generator = DeePSiMFc8() # 20_linear_03
+    elif variant == 'fc7':
+        generator = DeePSiMFc()  # 18_linear_02
+    else:
+        raise ValueError(f"Invalid variant {variant}")
+    
+    generator.load_state_dict(
+        state_dict=torch.load(path.join(weights_path, f'{variant}.pt'), map_location=device)
+    )
+    
+    generator.to(device=device)
+    generator.eval()
+    
+    return generator
+
+def generate(generator: nn.Module, activations: NDArray) -> Tensor:
+    
+    if type(generator) == InverseAlexGenerator:
+        out = generator(activations)
+    else:
+        out = generator(torch.tensor(activations))
+        
+    if type(out) == tuple:
+        out, _ = out
+        
+    return out
+
+def to_image(
+    img_out: Tensor,
+    means: Tuple[float, float, float] = (104.0, 117.0, 123.0), # RGB means
+    raw_scale: float = 255.
+    ) -> List[Image.Image]:
+    
+    mean : Tensor = torch.tensor(means).reshape(-1, 1, 1)
+
+    img_out += mean
+    img_out /= raw_scale
+    
+    to_pil = ToPILImage()
+    
+    img_list = [to_pil(img_out[i]) for i in range(img_out.shape[0])]
+
+    return img_list
+
+def main(network_name: str, variant: str, layer_name: str):
+    
+    # Loading `local_settings.json` for custom local settings
+    local_folder = path.dirname(path.abspath(__file__))
+    script_settings_fp = path.join(local_folder, 'local_settings.json')
+    script_settings: Dict[str, Any] = read_json(path=script_settings_fp)
+    
+    network = instantiate_network(
+        network_name=network_name,
+        layer_name=layer_name
+    )
+    
+    image = load_test_image(
+        image_path=script_settings['test_image'],
+        image_size=(256, 256)
+    )
+    
+    activations = get_activations(sbj_net=network, img=image)   
+    
+    generator1 = get_custom_generator(
+        weights_path = script_settings['inverse_alex_net'] ,
+        variant = variant
+    )
+    
+    generator2 = get_external_generator(
+        weights_path = script_settings['inverse_alex_net'] ,
+        variant = variant
+    )
+    
+    image_out1 = generate(generator=generator1, activations=activations[layer_name])
+    image_out2 = generate(generator=generator2, activations=activations[layer_name])
+    
+    images1 = to_image(image_out1)
+    images2 = to_image(image_out2)
+    
+    image_name = path.splitext(path.basename(script_settings['test_image']))[0]
+    
+    fp1 = path.join(script_settings['image_out'], f'{image_name}-custom.png')
+    fp2 = path.join(script_settings['image_out'], f'{image_name}-external.png')
+    
+    images1[0].save(fp1)
+    images2[0].save(fp2)
+    
+def main_print_layers(network_name: str):
+    
+    for name in  NetworkSubject(network_name=network_name).layer_names:
+        print(name)
+    
+"""
+00_conv2d_01
+01_relu_01
+02_maxpool2d_01
+03_conv2d_02
+04_relu_02
+05_maxpool2d_02
+06_conv2d_03
+07_relu_03
+08_conv2d_04
+09_relu_04
+10_conv2d_05
+11_relu_05
+12_maxpool2d_03
+13_adaptiveavgpool2d_01
+14_dropout_01
+15_linear_01
+16_relu_06
+17_dropout_02
+18_linear_02
+19_relu_07
+20_linear_03
+""" 
+
+if __name__ == "__main__":
+    
+    main(
+        network_name='alexnet',
+        variant='fc8',
+        layer_name='20_linear_03'
+    )
+    
+    """main(
+        network_name='alexnet',
+        variant='fc7',
+        layer_name='18_linear_02'
+    )"""
+    
+    #main_print_layers(network_name='alexnet')
