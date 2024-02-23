63a250370c5bd511ab1763e0256b9183700a23b7
Sebastiano Quintavalle <878500@stud.unive.it>
2024-02-23 10:58:25 +0100

    docs: model

--- zdream/model.py
+++ zdream/model.py
@@ -1,29 +1,84 @@
-# Type Aliases
-from dataclasses import dataclass
-from typing import Dict, List
-import numpy as np
-from numpy.typing import NDArray
+import logging
 
+import numpy as np
+from typing import Dict, List
+from numpy.typing import NDArray
+from dataclasses import dataclass
 from torch import Tensor
 
+# --- TYPE ALIASES ---
 
-Mask = List[bool]
-Stimuli = Tensor
+Mask = List[bool]   
+''' 
+Boolean mask associated to a set of stimuli, to indicating if they refer 
+to synthetic of natural images (True for synthetic, False for natural).
+'''     
+
 Codes = NDArray | Tensor
-StimuliScore = NDArray[np.float32]  # 1-dimensional array with the length of the batch assigning a score to each tested stimulus
-SubjectState = Dict[str, NDArray]   # State of a subject mapping each layer to its batch of activation
+'''
+Set of codes representing the images in a latent space.
+The first dimension of the tensor is the batch size.
+'''
 
+Stimuli = Tensor
+'''
+Set of visual stimuli.
+The first dimension of the tensor is the batch size.
+'''
+
+SubjectState = Dict[str, NDArray]
+'''
+Set of subject responses to a visual stimuli.
+The subject state can have multiple layers, whose name 
+is mapped to its specific activations in the form of a batched array.
+'''
+
+StimuliScore = NDArray[np.float32] 
+'''
+Set of scores associated to each stimuli in
+the form of a one-dimensional array.
+'''
+
+# --- MESSAGE ---
 
 @dataclass
 class Message:
+    '''
+    The dataclass is an auxiliary generic component that
+    is shared among the entire data-flow.
+    The aim of the class is to make different components communicate
+    through the data-passing of common object they all can manipulate.
+    '''
+    
     mask    : NDArray[np.bool_]
+    '''
+    Boolean mask associated to a set of stimuli indicating if they are
+    synthetic of natural images.
+    
+    NOTE: The mask has not `Mask` type because it's not a list but an array.
+          This is made to facilitate filtering operations that are primarily
+          applied to arrays.
+    '''
+    
     label   : List[str] | None = None
+    '''
+    List of labels associated to the set of stimuli.
+    '''
+    
 
+# --- Logger ---
 
+# TODO - evolve the Logger into an IOHandler to save/load any type of data.
 class Logger:
+    '''
+    Class responsible for logging in the three channels info, warn and error.
+    
+    NOTE: The logging methods can be easily overridden to log with other strategies 
+          and technologies.
+    '''
 
-    def info(self,  mess: str): print(f"INFO: {mess}")
+    def info(self,  mess: str): logging.info(mess)
 
-    def warn(self,  mess: str): print(f"WARN: {mess}")
+    def warn(self,  mess: str): logging.warn(mess)
 
-    def error(self, mess: str): print(f"ERR:  {mess}")
+    def error(self, mess: str): logging.error(mess)
