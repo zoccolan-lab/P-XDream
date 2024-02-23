9338acc59eaa57f6ea045ab8418d4aaf539c2098
Paolo Muratore <pmurator@sissa.it>
2024-02-23 18:48:31 +0100

    Minor change to test_generator

--- test/test_generator.py
+++ test/test_generator.py
@@ -269,7 +269,7 @@ class InverseAlexGeneratorTest(unittest.TestCase):
         stimuli_1, _ = generator(mock_inp, [True]*(self.num_gen))
         stimuli_2, _ = generator(mock_inp)
         
-        self.assertTrue(torch.allclose(stimuli_1, stimuli_2, atol=1e-4))
+        self.assertTrue(torch.allclose(stimuli_1, stimuli_2, atol=1e-6))
         
     def test_inconsistent_mask(self):
         
