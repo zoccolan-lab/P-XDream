d382a5b1aaff714a02a056cc2eaaeac3c03f3c59
Sebastiano Quintavalle <878500@stud.unive.it>
2024-04-19 16:34:30 +0200

    refactor: neural recordings arsg

--- script/NeuralRecording/run/run_single.py
+++ script/NeuralRecording/run/run_single.py
@@ -1,15 +1,13 @@
+from script.cmdline_args import Args
+from script.script_utils import run_single
+from script.NeuralRecording.args import ARGS
 from script.NeuralRecording.neural_recording import NeuralRecordingExperiment
-from script.NeuralRecording.parser import get_parser
 
-def main(args):
-
-    experiment = NeuralRecordingExperiment.from_args(args=args)
-    experiment.run()
 
 if __name__ == '__main__':
-    
-    parser = get_parser()
-    
+
+    parser = Args.get_parser(args=ARGS)
     args = vars(parser.parse_args())
     
-    main(args=args)
+    run_single(args=args, exp_type=NeuralRecordingExperiment)
+
