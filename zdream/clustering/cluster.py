e7ca3a82f1508076761d2f13e200f652f8033327
Sebastiano Quintavalle <878500@stud.unive.it>
2024-05-15 08:56:42 +0200

    clustering adj and rand

--- zdream/clustering/cluster.py
+++ zdream/clustering/cluster.py
@@ -12,6 +12,7 @@ from zdream.utils.io_ import read_json, save_json
 from zdream.utils.logger import Logger, SilentLogger
 from zdream.utils.types import ScoringUnit
 from typing import List
+import random
 
 class Cluster:
     ''' 
@@ -181,6 +182,71 @@ class Clusters:
         
         return Clusters(clusters=clusters)
     
+    @classmethod
+    def _aux_adj_rand_clusters(
+        cls,
+        n_clu: int,
+        elements_ids: List[int]
+    ):
+        clu_size = len(elements_ids) // n_clu
+        
+        # Break element ids in slices of n_clu elements
+        clu_labels = [
+            elements_ids[i*clu_size:] if\
+                    (i+1)*clu_size > len(elements_ids)\
+            else elements_ids[i*clu_size:(i+1)*clu_size]
+            for i in range(n_clu)
+        ]
+        
+        clusters = Clusters()
+        
+        for labels in clu_labels:
+            
+            labels_arr = np.array(labels, dtype=np.int32)
+            cluster = Cluster(labels=labels_arr)
+            clusters.add(cluster)
+        
+        return clusters
+    
+    @classmethod
+    def adjacent_clusters(
+        cls, 
+        n_clu: int, 
+        elements: int
+    ) -> Clusters:
+        '''
+        Generate a set of clusters with adjacent elements.
+
+        :param n_clu: Number of clusters.
+        :type n_clu: int
+        :param elements: Total number of elements.
+        :type elements: int
+        :return: Set of clusters with adjacent elements.
+        :rtype: Clusters
+        '''
+        
+        return cls._aux_adj_rand_clusters(n_clu, list(range(elements)))
+    
+    @classmethod
+    def random_clusters(
+        cls, 
+        n_clu: int, 
+        elements: int
+    ) -> Clusters:
+        '''
+        Generate a set of clusters with random elements.
+
+        :param n_clu: Number of elements in each cluster.
+        :type n_clu: int
+        :param elements: Total number of elements.
+        :type elements: int
+        :return: Set of clusters with random elements.
+        :rtype: Clusters
+        '''
+        
+        elements_ids = list(range(elements))
+        random.shuffle(elements_ids)
+        return cls._aux_adj_rand_clusters(n_clu, elements_ids)
     
     # --- MAGIC METHODS ---   
 
